8b04f61d397fed08c3a55a58a0b0e6f5022b9a09
Kayan107 <138879700+Kayan107@users.noreply.github.com>
2023-07-26 19:59:47 +0800

    Add files via upload

---
+++ q5-result.py
@@ -0,0 +1,11 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\User\Downloads\q5.py ====================
+The loan amount is :500
+loan amount not in the range of 1000 to 100000
+
+==================== RESTART: C:\Users\User\Downloads\q5.py ====================
+The loan amount is :1000
+The annual interest rate is:0.1
+interest rate not between 0.1 to 100%
