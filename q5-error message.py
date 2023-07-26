7655c79fc26ee3805702b609cf11e59e3f7f4c02
Kayan107 <138879700+Kayan107@users.noreply.github.com>
2023-07-26 20:07:18 +0800

    Add files via upload

---
+++ q5-error message.py
@@ -0,0 +1,11 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\User\Downloads\Q4.py ====================
+Please enter coordinate x1: 1
+Please enter coordinate y1: 1
+Please enter coordinate x2: 1
+Please enter coordinate y2: 1
+Please enter coordinate x3: 1
+Please enter coordinate y3: 11
+Does not form a triangle
