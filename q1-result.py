0a6bafdc44034bf3dfc21df65be8793b239cfa7a
Kayan107 <138879700+Kayan107@users.noreply.github.com>
2023-07-26 20:13:48 +0800

    Add files via upload

---
+++ q1-result.py
@@ -0,0 +1,6 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\User\Downloads\q1.py ====================
+Enter radius in feet : 100
+The radius you provided was 100.00 feet and the area is about 31415.93 sq feet
